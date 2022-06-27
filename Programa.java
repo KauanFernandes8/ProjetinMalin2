af88948a855c76c53e3de7d81fe2d2638582f199
KauanFernandes8 <cc21243@g.unicamp.br>
2022-06-27 15:13:10 -0300

    .

--- Programa.java
+++ Programa.java
@@ -1,15 +1,50 @@
+import static java.lang.Integer.parseInt;
+import java.io.*;
 public class Programa
 {
     public static void main (String[] args)
     {
-        try
+        int escolha;
+        Teclado teclado = new Teclado();
+        for(;;)
         {
-            Logradouro logradouro = (Logradouro)ClienteWS.getObjeto(Logradouro.class, "https://api.postmon.com.br/v1/cep", "13033205");
-            System.out.println (logradouro);
-        }
-        catch (Exception erro)
-        {
-            System.err.println (erro.getMessage());
+            try
+            {
+                System.out.println("\n Seletor de Opções. Faça sua escolha: ");
+                System.out.println("0 - Sair ");
+                System.out.println("1 - Incluir ");
+                System.out.println("2 - Excluir");
+                System.out.println("3 - Atualizar");
+                System.out.println("4 - Pesquisar");
+                System.out.println("5 - Ver todos");
+
+                escolha = teclado.getUmInt();
+                switch(escolha)
+                {
+                    case 0:
+                        System.exit(0);
+                        break;
+
+                    case 1:
+                    {
+                        try
+                        {
+                            System.out.print("Digite o cep do estádio: ");
+                            int cep = Teclado.getUmInt();
+
+                            System.out.print("Digite o nome do estádio: ");
+                            String nome = Teclado.getUmString();
+
+                            System.out.print("Digite o nome do estádio: ");
+                            String nome = Teclado.getUmString();
+                        }
+                    }
+                }
+            }
+            catch (error)
+            {
+
+            }
         }
     }
 }
