dc2d647ca7aa9597c0f2384dff7f1aabdf9f0398
KAUAN GUSTAVO DIAS FERNANDES <u21243@academico.cotuca.unicamp.br>
2022-08-13 10:14:38 -0300

    subindo

--- Programa.java
+++ Programa.java
@@ -16,12 +16,12 @@ public class Programa
             try
             {
                 System.out.println("\n Seletor de Opções. Faça sua escolha: ");
-                System.out.println("0 - Sair ");
                 System.out.println("1 - Incluir ");
-                System.out.println("2 - Excluir");
-                System.out.println("3 - Atualizar");
-                System.out.println("4 - Pesquisar");
-                System.out.println("5 - Ver todos");
+                System.out.println("2 - Pesquisar"); //o certo é pesquisar
+                System.out.println("3 - Atualizar"); //
+                System.out.println("4 - Excluir");
+                System.out.println("5 - Pesquisar todos");
+
 
                 opcao = teclado.getUmInt();
                 switch(opcao)
@@ -51,11 +51,12 @@ public class Programa
 
                              var estadio = new Estadio(cep, numero, telefone, nome, complemento);
                              ClienteWS.postObjeto(estadio, estadio.getClass(), "http://localhost:3000/estadios");
+                             break;
                         }
 
                         catch (Exception erro)
                         {
-                            System.out.println("Dando erro aqui!!");
+                            System.out.println("Problemas internos! Tente novamente mais tarde");
                             break;
                         }
                     }
@@ -71,7 +72,68 @@ public class Programa
                             String cep1 = String.valueOf(cep);
 
                             System.out.println(ClienteWS.getObjeto(Estadio.class, "http://localhost:3000/estadios",cep1));
+                            break;
+                        }
+                        catch (Exception erro)
+                        {
+                            System.out.println("Dando erro aqui!!");
+                            break;
+                        }
+                    }
+                    case 3:
+                    {
+                        try
+                        {
+                            System.out.print("Digite o cep do estádio;\n");
+                            cep = Teclado.getUmInt();
+                            System.out.print("Digite o numero do estádio;\n");
+                            numero = Teclado.getUmInt();
+                            System.out.print("Digite o telefone do estádio;\n");
+                            telefone = Teclado.getUmInt();
+                            System.out.print("Digite o nome do estádio;\n");
+                            nome = Teclado.getUmString();
+                            System.out.print("Complemento do estádio;\n");
+                            complemento = Teclado.getUmString();
 
+                            var estadio = new Estadio(cep, numero, telefone, nome, complemento);
+                            ClienteWS.putObjeto(Estadio.class, estadio, "http://localhost:3000/estadios", Integer.toString(cep));
+
+                            break;
+                        }
+                        catch (Exception erro)
+                        {
+                            System.out.println("Problemas internos! Tente novamente mais tarde");
+                            break;
+                        }
+                    }
+                    case 4:
+                    {
+                        try
+                        {
+                            System.out.print("Digite o cep do estádio que deseja excluir\n");
+                            System.out.print("cep: ");
+
+                            cep = Teclado.getUmInt() ;
+
+                            String cep1 = String.valueOf(cep);
+
+
+
+                            System.out.println(ClienteWS.deleteObjeto(Estadio.class, "http://localhost:3000/estadios",cep1));
+
+                            break;
+                        }
+                        catch (Exception erro)
+                        {
+                            System.out.println("Dando erro aqui!!");
+                            break;
+                        }
+                    }
+                    case 5:
+                    {
+                        try
+                        {
+                            ClienteWS.getObjetos(Estadio.class, "http://localhost:3000/estadios").forEach((o) -> System.out.println(o));
                         }
                         catch (Exception erro)
                         {
